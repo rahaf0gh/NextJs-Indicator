f46eb1cebc3d2b73016e0105e5921eb2dee2c55c
rahaf0gh <rahaf0alghamdi@gmail.com>
2025-05-09 14:30:04 +0300

    rahaf 4

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { Suspense } from 'react';
-// import ChartPage from '../components/ChartPage';
+import ChartPage from '../components/ChartPage';
 
-export default function Page({ChartPage}) {
+const Page = () => {
   return (
     <div className="p-8 space-y-8">
-      <Suspense>
-        {ChartPage}
+      <Suspense fallback={<p className="p-5">جاري تحميل الرسم البياني...</p>}>
+        <ChartPage />
       </Suspense>
     </div>
   );
 }
+
+export default Page;
