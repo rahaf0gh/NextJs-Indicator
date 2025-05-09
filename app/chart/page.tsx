0e15207da97fbdb801490838820e34d316456a50
rahaf0gh <rahaf0alghamdi@gmail.com>
2025-05-09 14:24:58 +0300

    ooo 3

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Suspense } from 'react';
-import ChartPage from '../components/ChartPage';
+// import ChartPage from '../components/ChartPage';
 
-export default function Page() {
+export default function Page({ChartPage}) {
   return (
     <div className="p-8 space-y-8">
       <Suspense>
-        <ChartPage />
+        {ChartPage}
       </Suspense>
     </div>
   );
